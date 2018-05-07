2e60de5fe3017ef2b5031c45ab9acb4b80f7bdfb
Elaine Anderson <eparie@bu.edu>
2018-05-07 18:44:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,20 +6,6 @@ def position_taken? (board, index)
     true
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+index = 0
 
-# code your input_to_index and move method here!
-
-def input_to_index (input)
-  input.to_i - 1
-end
-
-def move (array, index, character = "X")
-  array[index] = character
-end
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
