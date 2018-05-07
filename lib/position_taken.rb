90e4871e69574b7e09b131ff48e58345fad9bb33
Elaine Anderson <eparie@bu.edu>
2018-05-07 18:51:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if input == ["   "]
+  if index == 0
     false
   else
     true
 end
 
-index = 0
+
 
 board =["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
